36e6fb7411a3a376d3d7335afe9c728785044499
zsh <zhang1.61803398@foxmail.com>
2022-10-28 10:10:45 +0800

    modified:   backup.sh

--- backup.sh
+++ backup.sh
@@ -3,4 +3,5 @@ cd ~/zgo;
 DATECMD="date +%Y%m%d-%H%M%S"
 tar -zcvf backup/$($DATECMD).db.tar.gz zi.db
 tar -zcvf backup/$($DATECMD).8081.nohup.out.tar.gz 8081.nohup.out
+rm 8081.nohup.out
 perl restart.pl 8081
